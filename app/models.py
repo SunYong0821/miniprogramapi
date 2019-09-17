f457b6b001992eac86a15c831f47f8a0f486b1b7
skenoy <skenoy@gmail.com>
2019-09-17 09:01:29 +0800

    miniprogramapi init

---
+++ app/models.py
@@ -0,0 +1,60 @@
+# #用来创建数据库模型
+# from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# app = Flask(__name__)
+# app.config.from_pyfile('./config.py')
+# db = SQLAlchemy(app)
+
+
+from app import db, auth
+from flask import current_app
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
+    gender = db.Column(db.Integer)
+    city = db.Column(db.String(32))
+    province = db.Column(db.String(32))
+    country = db.Column(db.String(32))
+    img = db.Column(db.String(128))
+    openid = db.Column(db.String(32))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def to_json(self):
+        return {
+            'name': self.name,
+            'gender': self.gender,
+            'city': self.city,
+            'province': self.province,
+            'country': self.country,
+            'img': self.img,
+            'openid': self.openid,
+        }
+
+    # 获取token
+    def generate_auth_token(self):
+        #s = Serializer(current_app.config['SECRET_KEY'], expires_in=10)
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=3600)
+        return str(s.dumps({'openid': self.openid}), encoding='utf-8')
+
+
+@auth.verify_token
+def verify_token(token):
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        s.loads(token)
+    except SignatureExpired:
+        print('token过期')
+        return False
+    except BadSignature:
+        print('token不正确')
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    db.create_all()
---
+++ app/user/__init__.py
@@ -0,0 +1,46 @@
+# coding:utf-8
+from flask import Blueprint, jsonify, request, json
+from app.models import User
+from app import auth, db, app
+import requests
+
+user = Blueprint('user', __name__)
+
+
+@user.route("/login", methods=['POST'])
+def login():
+    resp = {'msg': 'info', 'data': {}, 'token': ''}
+
+    data = request.json['data']
+
+    wxurl = f'https://api.weixin.qq.com/sns/jscode2session?appid={app.config["MPID"]}&secret={app.config["MPKEY"]}&js_code={data["code"]}&grant_type=authorization_code'
+    req = requests.get(wxurl).text
+    openid = json.loads(req)['openid']
+
+    userinfo = User.query.filter_by(openid=openid).first()
+    if not userinfo:
+        r_user = User()
+        r_user.name = data['nickName']
+        r_user.gender = data['gender']
+        r_user.city = data['city']
+        r_user.province = data['province']
+        r_user.country = data['country']
+        r_user.img = data['avatarUrl']
+        r_user.openid = openid
+
+        db.session.add(r_user)
+        db.session.commit()
+
+        userinfo = r_user
+
+    resp['data'] = userinfo.to_json()
+    resp['msg'] = 'login / register success'
+    resp['token'] = userinfo.generate_auth_token()
+
+    return jsonify(resp)
+
+
+@user.route("/check_token", methods=['POST'])
+@auth.login_required
+def token():
+    return jsonify({'msg': 'success'})
