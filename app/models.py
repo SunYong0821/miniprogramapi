216ebac930dfee1c30f5d177d7bbe9b9cddc4a7f
skenoy <skenoy@gmail.com>
2019-09-17 17:45:20 +0800

    create database

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
-# #用来创建数据库模型
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# app = Flask(__name__)
-# app.config.from_pyfile('./config.py')
-# db = SQLAlchemy(app)
+# coding: utf-8
+##用来创建数据库模型
+#from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+#app = Flask(__name__)
+#app.config.from_pyfile('./config.py')
+#db = SQLAlchemy(app)
 
 
 from app import db, auth
