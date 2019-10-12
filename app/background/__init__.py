c5c63d35c974029363586077ebc5075e2f3c337a
skenoy <skenoy@gmail.com>
2019-10-12 15:57:05 +0800

    update bg init

--- app/background/__init__.py
+++ app/background/__init__.py
@@ -3,6 +3,9 @@ from flask import Blueprint
 
 bg = Blueprint('background', __name__)
 
+def show(url):
+    with open(url, 'rb') as 
+
 @bg.route('/helix')
 def helix():
     resp = Response(image, mimetype="image/jpeg")
