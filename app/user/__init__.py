1ce02690b741eca8b3d3cdda3bf895110bcea9c2
skenoy <skenoy@gmail.com>
2020-04-15 19:53:12 +0800

    fix check token update

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -46,4 +46,4 @@ def login():
 @user.route("/check_token")
 @auth.login_required
 def token():
-    return jsonify({'code':401, 'message': 'success'})
+    return jsonify({'code': 200, 'message': 'check token success'})
